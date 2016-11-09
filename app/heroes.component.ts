d51dd6ca1b03ade23d3634ea93d9583c3595502e
SUMETH KONGPRASERT <sumeth.kongprasert2@gmail.com>
2016-11-09 16:27:57 +0700

    step 21: Update the HeroesComponent class. (add action view hero detail)

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
@@ -14,11 +15,9 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService) { }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+  constructor(
+    private router: Router,
+    private heroService: HeroService) { }
 
   getHeroes(): void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
@@ -27,5 +26,14 @@ export class HeroesComponent implements OnInit {
   ngOnInit(): void {
     this.getHeroes();
   }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
 }
+
 
